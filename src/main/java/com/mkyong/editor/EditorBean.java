e84dbd595885d3cf60fe9f2f7101e282db181dfa
Moolerian <Moolerian@Gmail.com>
2018-11-08 19:49:40 +0330

    push_03 start the project at 17-08-1397

---
+++ src/main/java/com/mkyong/editor/EditorBean.java
@@ -0,0 +1,17 @@
+package com.mkyong.editor;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name = "editor")
+public class EditorBean {
+
+	private String value = "salaaaaaaaaaaaaaaaaaaaaaaam";
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+}
